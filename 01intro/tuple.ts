2630521ccc3f5635ef739f0913058a1c6e3fdc76
Ann <ann.suparada@gmail.com>
2022-11-15 17:51:22 +0700

    tuple

---
+++ 01intro/tuple.ts
@@ -0,0 +1,11 @@
+let user: [string, number, boolean]
+
+user = ['ann', 123, true]
+
+let rgb: [number, number, number]
+
+rgb = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser: User = [123, 'Ann']
