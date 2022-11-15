adb82ae21a84b6bb442502f1ecce17b41c0820b7
Ann <ann.suparada@gmail.com>
2022-11-15 15:18:51 +0700

    array

---
+++ 01intro/array.ts
@@ -0,0 +1,22 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUser: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255]
+]
+
+superHeros.push('spiderman')
+heroPower.push(2)
+
+allUser.push({name: 'Ann', isActive: true})
+MLModels.push([565, 565, 565])
+
+export {}
